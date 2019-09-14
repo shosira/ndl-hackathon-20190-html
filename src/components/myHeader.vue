32665ae2c9e44f09a10eadfc6460d822c0baa71f
Fumihiro Kato <fumi@fumi.me>
2019-09-15 02:24:24 +0900

    Moved query into myResult

--- src/components/myHeader.vue
+++ src/components/myHeader.vue
@@ -18,43 +18,6 @@
 </template>
 
 <script>
-import axios from 'axios'
-
-const queryPre = "https://jpsearch.go.jp/rdf/sparql/?default-graph-uri=&query="
-const speciesQuery = "SELECT+%3Fs+%3Flabel+%3Flat+%3Flong+WHERE+%7B%0D%0A++++%3Fs+rdf%3Atype%2Frdfs%3AsubClassOf*+type%3A%E6%A8%99%E6%9C%AC+%3B%0D%0A++++++++rdfs%3Alabel+%3Flabel++.%0D%0A++++%3Fs+jps%3Aspatial%2Fschema%3Ageo+%5B%0D%0A++++++++schema%3Alatitude+%3Flat%3B%0D%0A++++++++schema%3Alongitude+%3Flong%0D%0A++++%5D+.%0D%0A++++FILTER%28%3Flat+%3C+90%29%0D%0A++++FILTER%28bif%3Ast_within%28%0D%0A++++++++bif%3Ast_point%28%3Flong%2C+%3Flat%29%2C++%0D%0A++++++++bif%3Ast_point%28139.907700%2C+35.729130%29%2C+%0D%0A++++++++10+++++++++++++%0D%0A++++%29%29%0D%0A%7D&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on&run=+Run+Query+";
-
-function getCurrentPosition(options){
-  return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(resolve, reject, options)
-  })
-}
-
-function getItemsByPosition(latitude, longitude) {
-  /*const speciesUrl = queryPre + "SELECT+%3Fs+%3Flabel+%3Flat+%3Flong+WHERE+%7B%0D%0A++++%3Fs+rdf%3Atype%2Frdfs%3AsubClassOf*+type%3A%E6%A8%99%E6%9C%AC+%3B%0D%0A++++++++rdfs%3Alabel+%3Flabel++.%0D%0A++++%3Fs+jps%3Aspatial%2Fschema%3Ageo+%5B%0D%0A++++++++schema%3Alatitude+%3Flat%3B%0D%0A++++++++schema%3Alongitude+%3Flong%0D%0A++++%5D+.%0D%0A++++FILTER%28%3Flat+%3C+90%29%0D%0A++++FILTER%28bif%3Ast_within%28%0D%0A++++++++bif%3Ast_point%28%3Flong%2C+%3Flat%29%2C++%0D%0A++++++++bif%3Ast_point%28"
-    longitude +
-    "%2C" +
-    latitude +
-    "%29%2C+%0D%0A++++++++10+++++++++++++%0D%0A++++%29%29%0D%0A%7D&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on&run=+Run+Query+"*/
-  const speciesUrl = queryPre + speciesQuery
-  console.log(speciesUrl);
-  return axios
-    .get(speciesUrl)
-    .then(function(response) {
-      return response.data;
-    })
-}
-
-async function searchItemsByCurrentPosition(){
-  try {
-    let position = await getCurrentPosition();
-    console.log(position.coords.latitude, position.coords.longitude);
-
-    return getItemsByPosition(position.coords.latitude, position.coords.longitude);
-  }
-  catch(error){
-    console.log(error);
-  }
-}
 
 export default {
   name: 'myHeader',
@@ -67,17 +30,19 @@ export default {
           value: 0,
           marks: [0, 10, 30, 60, 100],
           result: null,
-          lat: 0,
-          long: 0
+          coords: [0, 0]
       }
   },
   methods: {
     searchByCurrentPosition: function() {
-      const _this = this;
-      searchItemsByCurrentPosition().then(function(response) {
-        console.log(response);
-        _this.$bus.emit('query-event', response);
-      });
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          function(position){
+            this.coords = position.coords;
+            this.$bus.emit('position-event', this.coords);
+          }.bind(this)
+        );
+      }
     }
   }
 
--- src/components/myResult.vue
+++ src/components/myResult.vue
@@ -24,7 +24,25 @@
 </template>
 
 <script>
+import axios from 'axios'
 import card from './card.vue'
+
+const queryPre = "https://jpsearch.go.jp/rdf/sparql/?default-graph-uri=&query="
+const speciesQuery = "SELECT+%3Fs+%3Flabel+%3Flat+%3Flong+WHERE+%7B%0D%0A++++%3Fs+rdf%3Atype%2Frdfs%3AsubClassOf*+type%3A%E6%A8%99%E6%9C%AC+%3B%0D%0A++++++++rdfs%3Alabel+%3Flabel++.%0D%0A++++%3Fs+jps%3Aspatial%2Fschema%3Ageo+%5B%0D%0A++++++++schema%3Alatitude+%3Flat%3B%0D%0A++++++++schema%3Alongitude+%3Flong%0D%0A++++%5D+.%0D%0A++++FILTER%28%3Flat+%3C+90%29%0D%0A++++FILTER%28bif%3Ast_within%28%0D%0A++++++++bif%3Ast_point%28%3Flong%2C+%3Flat%29%2C++%0D%0A++++++++bif%3Ast_point%28139.907700%2C+35.729130%29%2C+%0D%0A++++++++10+++++++++++++%0D%0A++++%29%29%0D%0A%7D&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on&run=+Run+Query+";
+
+function getItemsByCoords(coords) {
+  /*const speciesUrl = queryPre + "SELECT+%3Fs+%3Flabel+%3Flat+%3Flong+WHERE+%7B%0D%0A++++%3Fs+rdf%3Atype%2Frdfs%3AsubClassOf*+type%3A%E6%A8%99%E6%9C%AC+%3B%0D%0A++++++++rdfs%3Alabel+%3Flabel++.%0D%0A++++%3Fs+jps%3Aspatial%2Fschema%3Ageo+%5B%0D%0A++++++++schema%3Alatitude+%3Flat%3B%0D%0A++++++++schema%3Alongitude+%3Flong%0D%0A++++%5D+.%0D%0A++++FILTER%28%3Flat+%3C+90%29%0D%0A++++FILTER%28bif%3Ast_within%28%0D%0A++++++++bif%3Ast_point%28%3Flong%2C+%3Flat%29%2C++%0D%0A++++++++bif%3Ast_point%28"
+    longitude +
+    "%2C" +
+    latitude +
+    "%29%2C+%0D%0A++++++++10+++++++++++++%0D%0A++++%29%29%0D%0A%7D&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on&run=+Run+Query+"*/
+  const speciesUrl = queryPre + speciesQuery
+  return axios
+    .get(speciesUrl)
+    .then(function(response) {
+      return response.data;
+    })
+}
 
 export default {
   name: 'result',
@@ -42,7 +60,8 @@ export default {
          { name: '山田', age: '34歳' },
          { name: '田中', age: '45歳' }
       ],
-      result: null
+      result: null,
+      coords: [0, 0]
     }
   },
   computed: {
@@ -51,10 +70,12 @@ export default {
     }
   },
   mounted: function(){
-    const _this = this;
-    this.$bus.on('query-event', function(queryResult) {
-      _this.result = queryResult;
-    })
+    this.$bus.on('position-event', function(coords) {
+      this.coords = coords;
+      getItemsByCoords(coords).then(function(result) {
+        this.result = result;
+      }.bind(this));
+    }.bind(this));
   },
 }
 </script>
