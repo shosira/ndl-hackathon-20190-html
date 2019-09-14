0e815213543a03bead5c069a231f1c8658facbe5
Fumihiro Kato <fumi@fumi.me>
2019-09-15 04:51:49 +0900

    Changed query

--- src/components/myResult.vue
+++ src/components/myResult.vue
@@ -10,12 +10,13 @@
             </ul>
         </div>
     </div-->
-    <div id="result" v-if="result">
-        <div class="listbox">
-            <h3>生物</h3>
+    <div id="result">
+        <div class="listbox" v-for="category in categories" :key="category.name">
+            <h3>{{ category.name }}</h3>
             <ul>
-                <li class="card" v-for="record in result.results.bindings">
+                <li class="card" v-for="record in results.results.bindings">
                     <h3>{{ record.label.value }}</h3>
+                    <p>{{ record.image }}</p>
                     <p>{{ record.lat.value}}, {{ record.long.value }}</p>
                 </li>
             </ul>
@@ -34,11 +35,20 @@ const endpoint = new SparqlHttp({ endpointUrl: "https://jpsearch.go.jp/rdf/sparq
 
 function getItemsByCoords(coords, type) {
   const distance = "5";
-  let query = "SELECT ?s ?label ?lat ?long ";
-  query += "WHERE {";
-  query += " ?s rdfs:label ?label .";
-  query += " ?s rdf:type/rdfs:subClassOf* type:標本 .";
-  query += ` ?s jps:spatial/schema:geo [
+  let query = "SELECT ?s ?label ?lat ?long ?image ?type\n";
+  query += "WHERE {\n";
+  query += "  ?s rdfs:label ?label.\n";
+  query += "  VALUES ?type { "
+  type.types.forEach((value, i, array) => {
+    query += "type:" + value;
+    if (i < array.length - 1) {
+      query += " ";
+    }
+  });
+  query += "  }\n";
+  query += "  ?s rdf:type/rdfs:subClassOf* ?type .\n" ;
+  query += "  OPTIONAL { ?s schema:image ?image . } \n"
+  query += `  ?s jps:spatial/schema:geo [
       schema:latitude ?lat;
       schema:longitude ?long
     ] .
@@ -51,7 +61,7 @@ function getItemsByCoords(coords, type) {
    query += coords.latitude;
    query += "), "
    query += distance;
-   query += " )) }";
+   query += "  )\n)\n }";
 
   return endpoint.selectQuery(query)
     .then(res => res.text())
@@ -59,11 +69,6 @@ function getItemsByCoords(coords, type) {
       const result = JSON.parse(body);
       return result;
     }).catch(err => console.error(err));
-  //return axios
-  //  .get(speciesUrl)
-  //  .then(function(response) {
-  //    return response.data;
-  //  })
 }
 
 export default {
@@ -73,11 +78,11 @@ export default {
   },
   data: function() {
     return {
-      typeList: [
-        { id: 1, category: "生物", types: ["標本"] },
-        { id: 2, category: "工芸・装飾", types: ["コレクション", "工芸", "装飾・工芸", "金工・武器"] }
+      categories: [
+        { id: "cat1", name: "生物", types: ["標本"], result: []},
+        { id: "cat2", name: "工芸・装飾", types: ["コレクション", "工芸", "装飾・工芸", "金工・武器"], result: []}
       ],
-      result: null,
+      results: [],
       coords: [0, 0]
     }
   },
@@ -89,9 +94,11 @@ export default {
   mounted: function(){
     this.$bus.on('position-event', function(coords) {
       this.coords = coords;
-      getItemsByCoords(coords).then(function(result) {
-        this.result = result;
-      }.bind(this));
+      this.categories.forEach((name, types, result) => {
+        getItemsByCoords(this.coords, name).then(function(res) {
+          this.results = res;
+        }.bind(this))
+      });
     }.bind(this));
   },
 }
